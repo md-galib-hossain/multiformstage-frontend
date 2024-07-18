04560cdc511c7e2de90b9c3eab96a7196fe1c196
Md Galib Hossain <mdgalib23@gmail.com>
2024-07-18 11:08:27 +0600

    Feat: Initialize nextjs

---
+++ src/app/(withFirstLayout)/layout.tsx
@@ -0,0 +1,14 @@
+
+const layout = ({children} : {children : React.ReactNode}) => {
+  return (
+    <>
+    
+    <div className="min-h-screen">
+{children}
+    </div>
+    
+    </>
+  )
+}
+
+export default layout
---
+++ src/components/MultiStepForm/Steps.tsx
@@ -0,0 +1,10 @@
+
+const Steps = ({steps} : any) => {
+  return (
+    <div>
+        <h2>Steps</h2>
+    </div>
+  )
+}
+
+export default Steps
